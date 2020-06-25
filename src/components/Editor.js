1986a22eeee5eb4ef4c710445ed222f386ce59d7
Darren Potter <dalkion@hotmail.com>
2020-06-25 20:00:42 +0100

    first commit

---
+++ src/components/Editor.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import SimpleMDE from 'react-simplemde-editor';
+
+class Editor extends Component {
+	render() {
+		return (
+			<div className="editor column column-75">
+				<SimpleMDE
+					onChange={this.props.change}
+					value="Hello"
+					options={{ autofocus: true }}
+				/>
+			</div>
+		);
+	}
+}
+
+export default Editor;
